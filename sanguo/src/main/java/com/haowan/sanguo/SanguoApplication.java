fdd4574065649dec88a87973aa9ac198880125ab
9cb_100008504522_stoneFish <9cb_100008504522_stoneFish@git.cloud.tencent.com>
2020-08-20 23:31:33 +0800

    init commit

---
+++ sanguo/src/main/java/com/haowan/sanguo/SanguoApplication.java
@@ -0,0 +1,13 @@
+package com.haowan.sanguo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanguoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanguoApplication.class, args);
+    }
+
+}
---
+++ sanguo/src/test/java/com/haowan/sanguo/SanguoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haowan.sanguo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanguoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
